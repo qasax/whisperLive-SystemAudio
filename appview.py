427f0abb1592c50157cb24581cb8d4753cf090c4
abacoo <2961360533@qq.com>
2024-10-25 00:04:04 +0800

    基本实现字母功能

---
+++ appview.py
@@ -0,0 +1,33 @@
+import json
+import threading
+
+from whisper_live.client import TranscriptionClient
+
+
+class ScriptRunner:
+    def __init__(self):
+        self.output = []
+
+    def run_script(self):
+        client = TranscriptionClient(
+            "localhost",
+            9090,
+            lang="zh",
+            translate=False,
+            model="small",
+            use_vad=False,
+            save_output_recording=True,  # 仅用于麦克风输入，默认为 False
+            output_recording_filename="./output_recording.wav",  # 仅用于麦克风输入
+        )
+        client(wasapi=True)
+
+    def get_output(self):
+        return json.dumps(self.output)
+
+
+if __name__ == "__main__":
+    # window = webview.create_window('Woah dude!', 'MainFrame.html')
+    runner = ScriptRunner()
+    threading.Thread(target=runner.run_script, daemon=True).start()
+    #sleep(1000)
+    # webview.start()
--- desktopApp.py
+++ desktopApp.py
@@ -4,25 +4,43 @@ from whisper_live.server import TranscriptionServer
 import threading
 
 
-class WebClient:
+class InitServerAndClient:
     def __init__(self):
         self.client_thread = None
         self.message = None
         self.client = None
+        self.server_thread = None
+        self.server = None
+
+    def run_server(self, port, backend, faster_whisper_custom_model_path, whisper_tensorrt_path,
+                   trt_multilingual, single_model):
+        print('server running')
+        faster_whisper_custom_model_path = None if faster_whisper_custom_model_path == 'None' else faster_whisper_custom_model_path
+        whisper_tensorrt_path = None if whisper_tensorrt_path == 'None' else whisper_tensorrt_path
+        trt_multilingual = True if trt_multilingual == 'True' else False
+        single_model = True if single_model == 'True' else False
+        self.server = TranscriptionServer()
+        self.server_thread = threading.Thread(target=self.server.run(
+            "0.0.0.0",
+            int(port),
+            backend, faster_whisper_custom_model_path, whisper_tensorrt_path, trt_multilingual, single_model
+        ), args=("server thread", 1))
+        self.server_thread.daemon = True
+        self.server_thread.start()
 
     def run_client(self, port=9090, lang='zh'):
         self.client = TranscriptionClient(
             "localhost",
-            port=port,
+            port=int(port),
             lang=lang,
             translate=False,
             model="small",
             use_vad=False,
             save_output_recording=True,  # Only used for microphone input, False by Default
-            output_recording_filename="./output_recording.wav" , # Only used for microphone input
+            output_recording_filename="./output_recording.wav",  # Only used for microphone input
             msg_callback=self.set_message
         )
-        self.client_thread = threading.Thread(self.client(), args=("client thread", 2))
+        self.client_thread = threading.Thread(self.client(wasapi=True), args=("client thread", 2))
         self.client_thread.daemon = True
         self.client_thread.start()
 
@@ -33,28 +51,6 @@ class WebClient:
         self.message = msg
 
 
-class WebServer:
-
-    def __init__(self):
-        self.server_thread = None
-        self.server = None
-
-    def run_server(self, port, backend, faster_whisper_custom_model_path, whisper_tensorrt_path,
-                   trt_multilingual, single_model):
-        self.server = TranscriptionServer()
-        self.server_thread = threading.Thread(target=self.server.run(
-            "0.0.0.0",
-            port,
-            backend,
-            faster_whisper_custom_model_path,
-            whisper_tensorrt_path,
-            trt_multilingual,
-            single_model,
-        ), args=("server thread", 1))
-        self.server_thread.daemon = True
-        self.server_thread.start()
-
-
 def showSubtitle():
     subtitleWindow.show()
 
@@ -63,8 +59,10 @@ def on_start():
     subtitleWindow.hide()
 
 
-client = WebClient()
-mainWindow = webview.create_window('', url='MainFrame.html', js_api=client)
-subtitleWindow = webview.create_window('', url='MainFrame.html', frameless=True, easy_drag=True, js_api=client)
+initServerAndClient = InitServerAndClient()
+
+mainWindow = webview.create_window('', url='MainFrame.html', js_api=initServerAndClient)
+subtitleWindow = webview.create_window('', url='Subtitle.html', frameless=True, easy_drag=True,
+                                       js_api=initServerAndClient)
 mainWindow.expose(showSubtitle)
 webview.start(on_start)
